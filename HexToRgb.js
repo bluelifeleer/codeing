c8cae761049836ee5ff7b90c6e8059bf76bb3dbb
bluelifelee <thebulelife@163.com>
2015-10-27 13:41:23 +0800

    Update HexToRgb.js

--- HexToRgb.js
+++ HexToRgb.js
@@ -1,7 +1,17 @@
 <script type="text/javascript">
-	var str = "#C9F7C4";
-	var rgb = fToRgb(str);
-	console.log(rgb);
+	/**
+	 * 十六进制每两位表示一个颜色值，对应rgb(255,255,255)这种格式
+	 * 取十六进制颜色字符串的每两位取一次，用取到的两位值中的第一位在颜色中对应的值乘以１６加上第二位上的数字在颜色中对应的值便是当前两位代表的rgb中的一种颜色，
+	 * @param hex color string
+	 * @return rgbString
+	 * $using HexToRgb(hstr);
+	 * date 2015-10-27
+	 * author bluelife
+	 * email thebulelife@163.com
+	 */
+	var hexColorStr = "#C9F7C4";	//hex color string
+	var rgb = fToRgb(str);	//return rgbstring
+	console.log(rgb);		//parint rgb
 	function HexToRgb(hstr){
 		var newArr = [];
 		var arrToStr = "";
@@ -11,8 +21,8 @@
 			var splitStr = newStr.substr(parseInt(i*2),2);
 			var first = splitStr.substr(0,1).toLowerCase();
 			var last = splitStr.substr(1,1).toLowerCase();
-				newArr.push(parseInt(parseInt((fArr[first]*16))+parseInt(fArr[last])));
-			}
+			newArr.push(parseInt(parseInt((fArr[first]*16))+parseInt(fArr[last])));
+		}
 		for(var k=0;k<newArr.length;k++){
 			arrToStr+= newArr[k]+",";
 		}
